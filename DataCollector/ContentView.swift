0d513517b083eb1abbf0df019cbc670b2960d3bf
imran2205 <imrankabir1996@gmail.com>
2022-01-28 14:49:11 +0600

    Initial Commit

---
+++ DataCollector/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  DataCollector
+//
+//  Created by Imran Kabir on 28/1/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ DataCollector/DataCollectorApp.swift
@@ -0,0 +1,17 @@
+//
+//  DataCollectorApp.swift
+//  DataCollector
+//
+//  Created by Imran Kabir on 28/1/22.
+//
+
+import SwiftUI
+
+@main
+struct DataCollectorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
